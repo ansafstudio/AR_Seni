03d031c6722ccc00837443f9d24911efca5fce5c
Muhammad Anwar Safei <muhammad.anwar.safei@gmail.com>
2018-08-03 20:26:33 +0700

    implement
    implement

---
+++ Assets/Script/MateriMgr.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MateriMgr : MonoBehaviour {
+    public GameObject[] materi;
+    int index = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < materi.Length; i++)
+        {
+            if (index == i)
+            {
+                materi[i].SetActive(true);
+            }
+            else
+            {
+                materi[i].SetActive(false);
+            }
+        }
+	}
+
+    public void Next()
+    {
+        index = index + 1;
+        if (index == materi.Length)
+        {
+            index = 0;
+        }
+    }
+
+    public void Prev()
+    {
+        if (index == 0)
+        {
+            index = materi.Length;
+        }
+        index = index - 1;
+    }
+}
---
+++ Assets/Script/MenuMgr.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMgr : MonoBehaviour {
+    public GameObject MainMenu;
+    public GameObject PetunjukMenu;
+    public GameObject MateriMenu;
+    public GameObject PenugasanMenu;
+    public GameObject TentangMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartAR()
+    {
+        MainMenu.SetActive(false);
+        PetunjukMenu.SetActive(false);
+        MateriMenu.SetActive(false);
+        PenugasanMenu.SetActive(false);
+        TentangMenu.SetActive(false);
+    }
+
+    public void BackToMainMenu()
+    {
+        MainMenu.SetActive(true);
+        PetunjukMenu.SetActive(false);
+        MateriMenu.SetActive(false);
+        PenugasanMenu.SetActive(false);
+        TentangMenu.SetActive(false);
+    }
+
+    public void OpenEvaluasi()
+    {
+        Application.OpenURL("https://goo.gl/forms/YNaUYQMDFRheOWrM2");
+    }
+
+    public void OpenPetunjuk()
+    {
+        MainMenu.SetActive(false);
+        PetunjukMenu.SetActive(true);
+        MateriMenu.SetActive(false);
+        PenugasanMenu.SetActive(false);
+        TentangMenu.SetActive(false);
+    }
+
+    public void OpenMateri()
+    {
+        MainMenu.SetActive(false);
+        PetunjukMenu.SetActive(false);
+        MateriMenu.SetActive(true);
+        PenugasanMenu.SetActive(false);
+        TentangMenu.SetActive(false);
+    }
+
+    public void OpenPenugasan()
+    {
+        MainMenu.SetActive(false);
+        PetunjukMenu.SetActive(false);
+        MateriMenu.SetActive(false);
+        PenugasanMenu.SetActive(true);
+        TentangMenu.SetActive(false);
+    }
+
+    public void OpenTentang()
+    {
+        MainMenu.SetActive(false);
+        PetunjukMenu.SetActive(false);
+        MateriMenu.SetActive(false);
+        PenugasanMenu.SetActive(false);
+        TentangMenu.SetActive(true);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+    }
+}
